bf8cc98ee9498750767f56c891d951b939a8c47c
Yuval Banay <yuvalbanay@Yuvals-MacBook-Air.local>
2024-11-28 17:51:10 +0200

    completed

--- Primes.java
+++ Primes.java
@@ -1,5 +1,9 @@
 public class Primes {
     public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please provide a number.");
+            return;
+        }
         int n = Integer.parseInt(args[0]);
         int counter = 0;
         boolean[] isPrime = new boolean[n + 1];
